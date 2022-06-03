205b2ef62eb0c1ad8a89b23d73808eaa727c7bb1
IvayloTodorov <ivo_todorov.ab@abv.bg>
2022-06-03 10:55:59 +0300

    Implemented About us page

---
+++ ShopXWeb/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopXWeb.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class AboutController : Controller
+    {
+        public IActionResult Index() => this.View();
+    }
+}
